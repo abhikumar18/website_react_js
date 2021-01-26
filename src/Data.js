e2b308d0a6fd98a565056e3c9944e494c99140ce
Abhishek Kumar <abhishek.it.msit@gmail.com>
2021-01-26 21:34:14 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,108 @@
+export const storeProducts = [
+    {
+       id:1,
+       title:"Google Pixel - Black",
+       img:"images/pic_1.jpg",
+       price:10,
+       company:"Google",
+       info:
+         "Google announced the first generation Pixel smartphones, the Pixel and the Pixel XL, on October 4, 2016 during the #MadeByGoogle event.[1] Google emphasized the camera on the two phones, which ranked as the best smartphone camera on DxOMarkMobile with 90 points until HTC released the U11, which also scored 90 points.[2] This is largely due to software optimizations such as HDR+. The Pixel phones also include unlimited cloud storage for pictures on Google Photos[3] and, for devices purchased directly from Google, an unlockable bootloader.[4]",
+       inCart:false,
+       count:0,
+       total:0
+    },
+    {
+       id:2,
+       title:"Redmi - 8A",
+       img:"images/pic_2.jpg",
+       price:15,
+       company:"Redmi",
+       info:
+         "Redmi is a sub-brand owned by the Chinese electronics company Xiaomi. It was first announced in July 2013 as a budget smartphone line,[1] and became a separate sub-brand of Xiaomi in 2019 with entry-level and mid-range devices, while Xiaomi itself produces upper-range and flagship Mi phones. Redmi phones use the Xiaomi MIUI user interface on top of Android. Models are divided into Redmi (entry-level), Redmi Note and Pro (mid-range), and Redmi A (low-end), along with Redmi X (mid-range) and Redmi K (mid-range/high-end)",
+       inCart:false,
+       count:0,
+       total:0
+    },
+    {
+       id:3,
+       title:"Samsung - S7",
+       img:"images/pic_3.jpg",
+       price:10,
+       company:"Samsung",
+       info:
+         "Samsung Galaxy S7, Samsung Galaxy S7 Edge and Samsung Galaxy S7 Active are Android-based smartphones manufactured, released and marketed by Samsung Electronics. The S7 series serves as the successor to the Galaxy S6, S6 Edge, S6 Edge+ and S6 Active released in 2015. The S7 and S7 Edge were officially unveiled on 21 February 2016 during a Samsung press conference at Mobile World Congress, with a European and North American release on 11 March 2016.[8][9] The S7 Active was unveiled on 4 June 2016, and released on AT&T in the United States on 10 June 2016.[2]",
+       inCart:false,
+       count:0,
+       total:0
+    },
+    {
+       id:4,
+       title:"iPhone - 7S",
+       img:"images/pic_4.jpg",
+       price:20,
+       company:"Apple",
+       info:
+         "The iPhone 7 and iPhone 7 Plus are smartphones designed, developed, and marketed by Apple Inc. They are the tenth generation of the iPhone. They were announced on September 7, 2016, at the Bill Graham Civic Auditorium in San Francisco by Apple CEO Tim Cook, and were released on September 16, 2016, succeeding the iPhone 6S and iPhone 6S Plus as the flagship devices in the iPhone series. Apple also released the iPhone 7 and iPhone 7 Plus in numerous countries worldwide throughout September and October 2016. They were succeeded as flagship devices by the iPhone 8 and iPhone 8 Plus on September 22, 2017.",
+       inCart:false,
+       count:0,
+       total:0
+    },
+    {
+       id:5,
+       title:"Motorola - X",
+       img:"images/pic_5.jpg",
+       price:18,
+       company:"Motorola",
+       info:"Moto X is an Android smartphone developed and manufactured by Motorola Mobility, and released in August 2013.The Moto X was the first smartphone from the company fully developed under Google -[3] which acquired Motorola Mobility in 2012 - introduced with a brand refresh,[4] and a new flagship design language replacing the metallic and Kevlar coating used since the original Droid.[5] It was primarily aimed at consumers,[6] distinguished by features taking advantage of voice recognition (through Google Now)[7] and contextual awareness",
+       inCart:false,
+       count:0,
+       total:0
+    },
+    {
+      id:6,
+      title:"onePlus 8",
+      img:"images/pic_6.jpg",
+      price:2,
+      company:"One Plus Technology",
+      info:
+        "The 8 and 8 Pro are constructed similarly to previous OnePlus phones, using an anodized aluminum frame and curved Gorilla Glass 5 on both the front and back. Both have a circular cutout in the upper-left-hand corner for the front-facing camera; on the 8 Pro this replaces the pop-up camera used on the 7 Pro and 7T Pro. The rear camera module is similar to that of the 7 Pro and 7T Pro, protruding slightly from the back panel. On the 8, the dual-LED flash is located below, while on the 8 Pro, the telephoto camera, laser autofocus and dual-LED flash are all located to the left of the module. The 8 and 8 Pro are the first water-resistant OnePlus phones, rated at IP68. All 8 Pro models have water resistance, although for the 8 it is present only on carrier models.",
+      inCart:false,
+      count:0,
+      total:0
+   },
+   {
+    id:7,
+    title:"htc one m9",
+    img:"images/pic_7.jpg",
+    price:2,
+    company:"HTC",
+    info:"The HTC One M9+ is an Android smartphone manufactured and marketed by HTC which was announced on April 8, 2015.[1] Initially on launch, the device was only sold in China.[2] In July 2015, the device was released in Europe excluding the United Kingdom.[3]The device is an upscaled version of the One M9 where it comes with a larger screen and fingerprint sensor.[1]On September 29, 2015, HTC relaunched the One M9+ as the HTC One M9+ Supreme Camera Edition which features an upgraded camera module and it was released in Taiwan on October 6, 2015.[4]",
+    inCart:false,
+    count:0,
+    total:0
+ },
+ {
+  id:8,
+  title:"oppo a31",
+  img:"images/pic_8.jpg",
+  price:2,
+  company:"OPPO",
+  info:"On 3 August 2016, Oppo launched the F1s in India.[3] The Oppo F1s is a selfie-centric phone which succeeds the Oppo F1. The F1s sports a 13 MP primary camera at the back and has a 16 MP secondary camera on the front. There is a fingerprint scanner housed on the home button which can not only be used to unlock the phone but, open apps as well. The Oppo F1s is powered by MediaTek's MT6750 octa-core processor which is coupled with 3 GB RAM and Mali-T860 MP2 GPU. It runs on ColorOS 3.0 based on Android 5.1 Lollipop and features a 5.5-inch HD IPS display which is backed by 3075 mAh battery..",
+  inCart:false,
+  count:0,
+  total:0
+}];
+
+    export const detailProduct =
+    {
+      id:1,
+      title:"Google Pixel - Black",
+      img:"images/pic_1.jpg",
+      price:10,
+      company:"Google",
+      info:
+        "Here are some easy and engaging texts to practice and develop your German reading and comprehension skills. Written by experienced German language intitlestructors, these texts are specifically written to aid German students from the elementary and beginner A1 and A2 levels, as well as meeting the needs of the more advanced B1 and B2 level student",
+      inCart:false,
+      count:0,
+      total:0
+   };
